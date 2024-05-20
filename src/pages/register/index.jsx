4d20625755f186b35c0443f348fe51e71e875314
Gabriel Barbosa <gabrielbarbosaa432@gmail.com>
2024-05-20 08:50:02 -0300

    feat: add connection API

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,21 +1,45 @@
+/* eslint-disable no-unused-vars */
 import { Main, Body, Logo, Spacing, CreatAcc } from "./styles";
 import { Input, Button } from "../../components/";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const save = async (ev) => {
+    ev.preventDefault();
+    await axios.post("http://localhost:3333/user", data);
+    alert("Usuário criado com sucesso");
+  };
+
   return (
     <Main>
       <Body>
-        <form>
+        <form onSubmit={save}>
           <Logo>Crie sua conta!</Logo>
-          <Input label="Nome" placeholder="Digite seu nome" required={true} />
+          <Input
+            label="Nome"
+            placeholder="Digite seu nome"
+            required={true}
+            value={data.name}
+            onChange={(ev) => setData({ ...data, name: ev.target.value })}
+          />
           <Spacing></Spacing>
           <Input
             label="Email"
             placeholder="Digite o email"
             type="email"
             required={true}
+            value={data.email}
+            onChange={(ev) => setData({ ...data, email: ev.target.value })}
           />
           <Spacing></Spacing>
           <Input
@@ -23,6 +47,8 @@ const Register = () => {
             placeholder="Digite sua senha"
             type="password"
             required={true}
+            value={data.password}
+            onChange={(ev) => setData({ ...data, password: ev.target.value })}
           />
           <Spacing></Spacing>
           <Button value="Cadastrar" variant="btn-primary" type="submit" />
